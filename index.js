4f862e58619120388c3788bf68c976f995d2c13e
Muhammad-Yousuf3 <coolmaster808@gmail.com>
2024-05-06 04:59:21 -0700

    CLI number guessing game

---
+++ index.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.red("\n\t\tWelcome to Yousuf's Number Guessing Game\n"));
+let RandomNumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([
+    { name: "Guess the number", type: "number", message: chalk.blue("Guess any number from 1 to 6") }
+]);
+if (RandomNumber === answer.name) {
+    console.log(chalk.green("Congrats!You guessed it Right."));
+}
+else {
+    console.log(chalk.yellow("TryAgain!"));
+    console.log(chalk.greenBright("You guessed it Wrong!"));
+}
